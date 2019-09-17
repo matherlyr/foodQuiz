b52ff9597f13753445cd2f9efe9b015123aaea40
Rachel K. Matherly <rachel.matherly@centre.edu>
2019-09-17 15:47:03 -0400

    whatever

--- RNC.js
+++ RNC.js
@@ -1,1 +1,13 @@
+var sayHi = function  ()
+{
+var node= document.createElement("h2")
+node.innerText="Welcome!"
 
+var body=document.querySelector("body")
+    body.appendChild(node) ;
+    
+ document.querySelector("#button1")
+    .setAttribute("onClick",null);
+    
+    
+}
