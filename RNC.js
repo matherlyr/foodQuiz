960e9834597e2d2a996e75eae6020df843f48481
matherlyr <54860556+matherlyr@users.noreply.github.com>
2019-09-18 22:32:49 -0400

    Update RNC.js

--- RNC.js
+++ RNC.js
@@ -1,7 +1,7 @@
 var sayHi = function  ()
 {
 var node= document.createElement("h2")
-node.innerText="Welcome!"
+node.innerText="Gina went to the  grocery store. She bought ingredients to make a lasagna. She bought noodles, pasta sauce, hamburger meat, tomatoes, and cheese."
 
 var body=document.querySelector("body")
     body.appendChild(node) ;
@@ -9,5 +9,30 @@ var body=document.querySelector("body")
  document.querySelector("#button1")
     .setAttribute("onClick",null);
     
+    
+}
+var macncheeseButton = function  ()
+{
+var node= document.createElement("h2")
+node.innerText="She took all her ingredients home and started cooking. First, she browned her hamburger meat and cooked the noodles. Then, she started layering the lasagna. And finally, she put it in the oven."
+
+var body=document.querySelector("body")
+    body.appendChild(node) ;
+    
+ document.querySelector("#button2")
+    .setAttribute("onClick",null);
+    
+    
+}
+var spaghettiButton = function  ()
+{
+var node= document.createElement("h2")
+node.innerText="When she got it out of the oven, it smelled wonderful! She set it on the kitchen table and her family enjoyed it!"
+
+var body=document.querySelector("body")
+    body.appendChild(node) ;
+    
+ document.querySelector("#button3")
+    .setAttribute("onClick",null);
     
 }
